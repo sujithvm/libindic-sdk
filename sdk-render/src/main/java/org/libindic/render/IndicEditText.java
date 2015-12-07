a96836de2364b9feac8fc05429725c550ba6c013
sujith <sujithvm24@gmail.com>
2015-12-07 21:42:53 +0530

    add sdk render

---
+++ sdk-render/src/main/java/org/libindic/render/IndicEditText.java
@@ -0,0 +1,22 @@
+package org.libindic.render;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * Created by sujith on 7/12/15.
+ */
+public class IndicEditText extends EditText {
+    public IndicEditText(Context context) {
+        super(context);
+    }
+
+    public IndicEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public IndicEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ sdk-render/src/main/java/org/libindic/render/IndicTextView.java
@@ -0,0 +1,22 @@
+package org.libindic.render;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by sujith on 7/12/15.
+ */
+public class IndicTextView extends TextView {
+    public IndicTextView(Context context) {
+        super(context);
+    }
+
+    public IndicTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public IndicTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
